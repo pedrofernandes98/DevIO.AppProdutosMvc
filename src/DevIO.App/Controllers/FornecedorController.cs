2850837fff73839a466e6144411add95f60493da
Pedro Fernandes <ppedrohfs@gmail.com>
2022-01-05 02:19:31 -0300

    Criando a Controller e as Views de Fornecedores

---
+++ src/DevIO.App/Controllers/FornecedorController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DevIO.App.Data;
+using DevIO.App.ViewModels;
+using DevIO.Business.Models;
+using DevIO.Business.Interfaces;
+using AutoMapper;
+
+namespace DevIO.App.Controllers
+{
+    public class FornecedorController : BaseController
+    {
+        private readonly IFornecedorRepository _repository;
+        private readonly IMapper _mapper;
+
+        public FornecedorController(IFornecedorRepository repository,
+                                      IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(_mapper.Map<IEnumerable<FornecedorViewModel>>(await _repository.GetAll()));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Details(Guid id)
+        {
+            var fornecedorViewModel = await GetFornecedorEndereco(id);
+
+            if (fornecedorViewModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(fornecedorViewModel);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(FornecedorViewModel fornecedorViewModel)
+        {
+            if (!ModelState.IsValid)
+                return View(fornecedorViewModel);
+
+            await _repository.Add(_mapper.Map<Fornecedor>(fornecedorViewModel));
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            var fornecedorViewModel = _mapper.Map<FornecedorViewModel>(await GetFornecedorProdutosEndereco(id));
+
+            if (fornecedorViewModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(fornecedorViewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, FornecedorViewModel fornecedorViewModel)
+        {
+            if (id != fornecedorViewModel.Id)
+                return NotFound();
+
+            if (!ModelState.IsValid)
+                return View(fornecedorViewModel);
+
+            await _repository.Edit(_mapper.Map<Fornecedor>(fornecedorViewModel));
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var fornecedorViewModel = await GetFornecedorProdutosEndereco(id);
+
+            if (fornecedorViewModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(fornecedorViewModel);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var fornecedorViewModel = await GetFornecedorProdutosEndereco(id);
+
+            if (fornecedorViewModel == null)
+            {
+                return NotFound();
+            }
+
+            await _repository.Remove(id);
+            return RedirectToAction(nameof(Index));
+        }
+
+        private async Task<FornecedorViewModel> GetFornecedorEndereco(Guid id)
+        {
+            return _mapper.Map<FornecedorViewModel>(await _repository.ObterFornecedorEndereco(id));
+        }
+
+        private async Task<FornecedorViewModel> GetFornecedorProdutosEndereco(Guid id)
+        {
+            return _mapper.Map<FornecedorViewModel>(await _repository.ObterFornecedorProdutosEndereco(id));
+        }
+    }
+}
