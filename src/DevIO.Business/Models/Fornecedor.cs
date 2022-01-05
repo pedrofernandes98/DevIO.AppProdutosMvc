604b1b8acf71031c1832384e4bd3bcf5eb702cc1
Pedro Fernandes <ppedrohfs@gmail.com>
2022-01-05 00:43:52 -0300

    Definindo os modelos da aplicacao

---
+++ src/DevIO.Business/Models/Fornecedor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace DevIO.Business.Models
+{
+    public class Fornecedor : Entity
+    {
+        public string Nome { get; set; }
+
+        public string Documento { get; set; }
+
+        public TipoFornecedor TipoFornecedor { get; set; }
+
+        public Endereco Endereco { get; set; }
+
+        public bool Ativo { get; set; }
+
+        //EF Releations
+        public IEnumerable<Produto> Produtos;
+    }
+}
---
+++ src/DevIO.Business/Models/Produto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DevIO.Business.Models
+{
+    public class Produto
+    {
+        //Foreign key
+        public Guid FornecedorId { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Descricao { get; set; }
+
+        public string Imagem { get; set; }
+
+        public decimal Valor { get; set; }
+
+        public DateTime DataCadastro { get; set; }
+
+        public bool Ativo { get; set; }
+
+        //EF Releations -> Propriedades de navegação
+        public Fornecedor Fornecedor { get; set; }
+    }
+}
